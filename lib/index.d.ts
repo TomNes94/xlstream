6aea744ac037c3b5d90924d39b72906e3f4b4705
Tom van Nes <tom@valuemetrics.nl>
2020-01-24 15:29:56 +0100

    Include lib

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+import { Transform } from 'stream';
+import { IXlsxStreamOptions, IWorksheetOptions } from './types';
+export declare function getXlsxStream(options: IXlsxStreamOptions): Promise<Transform>;
+export declare function getWorksheets(options: IWorksheetOptions): Promise<string[]>;
